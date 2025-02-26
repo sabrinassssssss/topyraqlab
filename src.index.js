a88065187760b93c2a8f45ed8f8d55ab219f1d4b
sabrinassssssss <isinasabrina@gmail.com>
2025-02-26 17:08:42 +0500

    Create src.index.js

---
+++ src.index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SmartGardenApp from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SmartGardenApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
