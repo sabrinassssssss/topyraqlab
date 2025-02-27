7ad156c217541b0f76a2d4d793675be434f0c70a
sabrinassssssss <isinasabrina@gmail.com>
2025-02-27 15:56:29 +0500

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SmartGardenApp from "./App";
-import "bootstrap/dist/css/bootstrap.min.css"; // Подключаем Bootstrap
-
-ReactDOM.render(<SmartGardenApp />, document.getElementById("root"));
